37062f0ad1aa96f1cdd30a9b1fd501f1912818c3
Janghyeonsuk <vkdnjdjxor@naver.com>
2022-08-07 16:04:13 +0900

    AppConfig code refactoring commit

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,11 +12,22 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
     // 생성자를 통해 Dependency Injection(의존관계 주입)
+    // 리팩토링을 통해서 역할을 세우고 구현이 들어가도록 설계
+    // 역할과 구현 클래스가 한눈에 확인, 중복 제거
+
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
