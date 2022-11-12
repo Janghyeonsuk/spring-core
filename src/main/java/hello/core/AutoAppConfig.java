25a551265d0cb8872a2d3181473572c36806bcfc
janghyeonsuk <vkdnjdjxor@naver.com>
2022-11-13 01:43:02 +0900

    최신 코드 추가

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,23 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration //설정 정보
+@ComponentScan ( //@Component 어노테이션이 붙은 클래스를 스캔해서 자동으로 스프링 빈으로 등록한다.
+        basePackages = "hello.core",
+//        basePackageClasses = AutoAppConfig.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) //컴포넌트 스캔에서 제외할것
+)
+public class AutoAppConfig {
+
+    //자동 bean 주입과 수동 bean 주입 충돌 ->
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,11 +1,16 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
 
     //    MemberRepository(추상화), MemoryMemberRepository(구체화)에 모두 의존 -> DIP 원칙 위배
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
     private final MemberRepository memberRepository;
 
+    @Autowired //ac.getBean(MemberRepository.class) 생성자에 자동 의존관계 주입
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
